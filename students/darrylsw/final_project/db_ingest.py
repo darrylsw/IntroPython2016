521b69f6f61d33ae92e69540437bb8a734ee7034
Darryl Wong <darrylsw@gmail.com>
2016-12-11 21:46:28 -0800

    Final submission of final project

--- students/darrylsw/final_project/db_ingest.py
+++ students/darrylsw/final_project/db_ingest.py
@@ -1,31 +1,100 @@
 #!/usr/bin/env python3
 
-import pandas as pd 
-import time
+
+"""
+takes a csv listing all the datastore information from our vmware clusters such as storage used, free, capacity and 
+vms on each, sum up data based upon cluster and types of storage and import into a time series database, Influxdb. 
+This was what I discussed in my lightning talk toward the beginning of the quarter. The data will be used by Grafana 
+for different types of graphs.
+
+Importing of data into the database isn't functioning yet. The script is using classes so it can "easily" be expanded
+to accommodate other types of data such as vm and host counts, grouping by site, operating system, hardware,...
+
+"""
+
+import pandas as pd
 import os
 
-from influxdb import DataFrameClient
+from influxdb import DataFrameClient, InfluxDBClient
 
-def get_data(input_file):
-	pdata = pd.read_csv(input_file)
-	pdata['Date'] = os.path.getctime(input_file)
-	pdata.set_index(['Date'])
-	return pdata
+class df:
+	"""
+	dataframe class for default data
+	"""
+	tag_team = "vpt"
 
-def update_data (pdata):
-	pdata ['Used'] = pdata ['Capacity'] - pdata ['Free']
-	pdata ['Capacity_tb'] = pdata['Capacity'] / 1024
-	pdata ['Allocated_tb'] = pdata['Allocated'] / 1024
-	pdata ['Used_tb'] = pdata['Used'] / 1024
-	return pdata
+	def __init__(self, input_file):
+		pdata = pd.read_csv(input_file)
+		ctime = os.path.getctime(input_file)
+	
+		# special actions for storage data
+		if self.tag_object == "storage":
+			pdata ['Used'] = pdata ['Capacity'] - pdata ['Free']
+			pdata ['Capacity_tb'] = pdata['Capacity'] / 1024
+			pdata ['Allocated_tb'] = pdata['Allocated'] / 1024
+			pdata ['Used_tb'] = pdata['Used'] / 1024
+		
+			tmp_pdata = pdata.loc[pdata['Type'] == "VMFS"]
+			self.vmfs_data = tmp_pdata.groupby(['Cluster']).sum()
+			self.vmfs_data['Used_pct'] = self.vmfs_data['Used'] / self.vmfs_data['Capacity']
+			self.vmfs_data['Allocated_pct'] = self.vmfs_data['Allocated'] / self.vmfs_data['Capacity']
+			self.vmfs_data['Date'] = ctime
+			self.vmfs_data.set_index(['Date'])
 
-def group_data (pdata, dstype):
-	tmp_data = pdata.loc[pdata['Type'] == dstype]
-	group_data = tmp_data.groupby(['Cluster']).sum()
-	group_data['Used_pct'] = group_data['Used'] / group_data['Capacity']
-	group_data['Allocated_pct'] = group_data['Allocated'] / group_data['Capacity']
-	return group_data
+			tmp_pdata = pdata.loc[pdata['Type'] == "NFS"]
+			self.nfs_data = tmp_pdata.groupby(['Cluster']).sum()
+			self.nfs_data['Used_pct'] = self.nfs_data['Used'] / self.nfs_data['Capacity']
+			self.nfs_data['Allocated_pct'] = self.nfs_data['Allocated'] / self.nfs_data['Capacity']
+			self.nfs_data['Date'] = ctime
+			self.nfs_data.set_index(['Date'])
+		else: # our generic data
+			self.data=pdata
+			self.data['Date'] = ctime
+			self.data.set_index(['Date'])
 
+class storage_df (df):
+	"""
+	used for storage data 
+	"""
+	tag_object = "storage"
+
+	def create_json_list (self):
+		""""
+		for storage data we want to track:
+			used_tb, allocated_tb, capacity_tb, used_pct, allocated_pct
+		"""
+		json_list = []
+		for tag_dstype in ["VMFS", "NFS"]:
+			for cluster, row in self.vmfs_data.iterrows():
+				json_used = create_json ("Used_tb", self.tag_team, self.tag_object, tag_dstype, cluster, int(row['Date']), row['Used_tb'])
+				json_list.append (json_used)
+
+				json_allocated = create_json ("Allocated_tb", self.tag_team, self.tag_object, tag_dstype, cluster, int(row['Date']), row['Allocated_tb'])
+				json_list.append (json_allocated)
+
+				json_capacity = create_json ("Capacity_tb", self.tag_team, self.tag_object, tag_dstype, cluster, int(row['Date']), row['Capacity_tb'])
+				json_list.append (json_capacity)
+
+				json_used_pct = create_json ("Used_pct", self.tag_team, self.tag_object, tag_dstype, cluster, int(row['Date']), row['Used_pct'])
+				json_list.append (json_used_pct)
+
+				json_allocated_pct = create_json ("Allocated_pct", self.tag_team, self.tag_object, tag_dstype, cluster, int(row['Date']), row['Allocated_pct'])
+				json_list.append (json_allocated_pct)
+		return json_list
+
+class vm_df (df):
+	"""
+	future class for vm data
+	"""
+	tag_object = "vm"
+
+class host_df (df):
+	"""
+	future class for host data
+	"""
+	tag_object = "host"
+
+		
 def create_json (measurement, team, objecttype, dstype, cluster, date, val):
 	json = [
 	 	{
@@ -36,19 +105,19 @@ def create_json (measurement, team, objecttype, dstype, cluster, date, val):
 	 			"dstype": dstype,
 	 			"cluster": cluster
 	 		},
-	 		"time": date,
+	 		"time": int(date),
 	 		"fields": {
 	 			"value": val
 	 		}
 	 	}
  	]
+	return json
 
-
-def import2db (pdata):
-	tag_team = "vpt"
-	tag_object = "storage"
-	#tag_type = dstype
-
+def import2db (json_list):
+	"""
+	take a list of json data and import it into our database
+	Work in progress
+	"""
 
 	host = "localhost"
 	port = "8086"
@@ -56,24 +125,24 @@ def import2db (pdata):
 	password = ""
 	dbname = "db"
 
-	#for index, row in pdata.iterrows():
-		#json_used = create_json ("Used_tb", tag_team, tag_object, row['Type'], row['Cluster'], row['Date'], row['Used_tb'])
-		#json_allocated = create_json ("Allocated_tb", tag_team, tag_object, row['Type'], row['Cluster'], row['Date'], row['Allocated_tb'])
-		#json_capacity = create_json ("Capacity_tb", tag_team, tag_object, row['Type'], row['Cluster'], row['Date'], row['Capacity_tb'])
-		#json_used_pct = create_json ("Used_pct", tag_team, tag_object, row['Type'], row['Cluster'], row['Date'], row['Used_pct'])
-		#json_allocated_pct = create_json ("Allocated_pct", tag_team, tag_object, row['Type'], row['Cluster'], row['Date'], row['Allocated_pct'])
-		#print (json_used)
+	client = InfluxDBClient(host, port, user, password, dbname)
+
+	for json in json_list:
+		pass
+		#print (json)
+		# work in progress
+		#client.write_points(json_used)
+		#client.write_points(json_allocated)
+		#client.write_points(json_capacity)
+		#client.write_points(json_used_pct)
+		#client.write_points(json_allocated_pct)
 
 def main():
-	all_data = get_data ('test-data/datastore_20161207.csv')
-
-	all_data = update_data (all_data)
-
-	nfs_data = group_data (all_data, "NFS")
-	vmfs_data = group_data (all_data, "VMFS")
-
-
+	storage_df_data = storage_df('test-data/datastore_20161207.csv')
 
 if __name__ == '__main__':
 	main()
+	"""
+	eventually want the python script to take the filename as an argument
+	"""
 
--- students/darrylsw/final_project/test_ingest.py
+++ students/darrylsw/final_project/test_ingest.py
@@ -3,55 +3,56 @@ Test db_ingest.py
 """
 
 import os
-from db_ingest import get_data, update_data, group_data, import2db
+from db_ingest import create_json, df, storage_df, import2db
 
 filename = './test-data/datastore_20161207.csv'
-#ds_df = get_data('./test-data/datastore_20161207.csv')
 
 def test_get_data():
-	ds_df = get_data(filename)
-	assert "Cluster" in ds_df.head(1)
-	assert "Datastore" in ds_df.head(1)
-	assert 20 == ds_df['Cluster'].count()
-	assert "1.48141" in ds_df.head(1).to_string()
-	print
-
-def test_update_data():
-	ds_df = get_data(filename)
-	update_df = update_data(ds_df)
-	assert "500" in update_df['Used'].head(1).to_string()
-	assert "0.976562" in update_df['Capacity_tb'].head(1).to_string()
-	assert "0.78125" in update_df['Allocated_tb'].head(1).to_string()
-	assert "0.488281" in update_df['Used_tb'].head(1).to_string()
+	ds_df = storage_df(filename)
+	assert "Capacity" in ds_df.vmfs_data
+	assert "1.4814" in ds_df.vmfs_data['Date'].head(1).to_string()
 
 def test_group_data():
-	ds_df = get_data(filename)
-	update_df = update_data(ds_df)
-	group_df = group_data(update_df, "VMFS")
-	
-	assert "7.81" in group_df['Capacity_tb'].head(1).to_string()
-	assert "4.0039" in group_df['Allocated_tb'].head(1).to_string()
-	assert "5.8593" in group_df['Used_tb'].head(1).to_string()
-	assert ".75" in group_df['Used_pct'].head(1).to_string()
-	assert ".51" in group_df['Allocated_pct'].head(1).to_string()
+	ds_df = storage_df(filename)
+
+	assert "vpt" == ds_df.tag_team 
+	assert "storage" == ds_df.tag_object
+
+	assert "7.8125" in ds_df.vmfs_data['Capacity_tb'].head(1).to_string()
+	assert "4.0039" in ds_df.vmfs_data['Allocated_tb'].head(1).to_string()
+	assert "5.8593" in ds_df.vmfs_data['Used_tb'].head(1).to_string()
+	assert ".75" in ds_df.vmfs_data['Used_pct'].head(1).to_string()
+	assert ".51" in ds_df.vmfs_data['Allocated_pct'].head(1).to_string()
+
+	assert "2.441406" in ds_df.nfs_data['Capacity_tb'].head(1).to_string()
+	assert "2.246094" in ds_df.nfs_data['Allocated_tb'].head(1).to_string()
+	assert "0.488281" in ds_df.nfs_data['Used_tb'].head(1).to_string()
+	assert "0.2" in ds_df.nfs_data['Used_pct'].head(1).to_string()
+	assert "0.92" in ds_df.nfs_data['Allocated_pct'].head(1).to_string()
+
+
+def test_create_json():
+
+	json = create_json('Capacity_tb', 'vpt', 'storage', 'VMFS', 'cluster1', 1481415659.0, 1000)
+
+	assert "Capacity_tb" in json[0]['measurement']
+	assert "vpt" in json[0]['tags']['team']
+	assert "storage" in json[0]['tags']['object']
+	assert "VMFS" in json[0]['tags']['dstype']
+	assert "cluster1" in json[0]['tags']['cluster']
+	assert 1481415659.0 == json[0]['time']
+	assert 1000 == json[0]['fields']['value']
+
 
 def test_import2db():
-	ds_df = get_data(filename)
-	update_df = update_data(ds_df)
-	group_df_vmfs = group_data(update_df, "VMFS")
+	ds_df = storage_df(filename)
+	storage_json_list = ds_df.create_json_list()
+	import2db(storage_json_list)
 
-	import2db(group_df_vmfs)
-	assert False
-
+	
 
 
-# def test_add_date():
-# 	ds_df = get_data('./test-data/datastore_20161207.csv')
-# 	date=add_date('./test-data/datastore_20161207.csv', ds_df)
-# 	print (date)
 
-# 	now=add_date('./test-data/datastore_20161207.csv', ds_df, "12-1-2016")
-# 	assert False
 
 
 
